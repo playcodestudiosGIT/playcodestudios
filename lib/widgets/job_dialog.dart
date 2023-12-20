baf9686e04499f285b14b49a00b5e2154a35e295
playcode <playcodestudios@outlook.com>
2023-12-20 13:39:11 -0500

    swipers

--- lib/widgets/job_dialog.dart
+++ lib/widgets/job_dialog.dart
@@ -29,78 +29,133 @@ class _JobDialogState extends State<JobDialog> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              if (widget.trabajo.imgs.isNotEmpty)
-                AspectRatio(
-                    aspectRatio: 16 / 9,
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 450,
-                      child: Swiper(
-                        autoplay: true,
-                        autoplayDelay: 5000,
-                        itemWidth: 400,
-                        scrollDirection: Axis.horizontal,
-                        itemCount: widget.trabajo.imgs.length,
-                        pagination: const SwiperPagination(
-                            builder: DotSwiperPaginationBuilder(
-                                activeColor: primaryColor,
-                                color: secondaryColor)),
-                        itemBuilder: (context, index) {
-                          final img = widget.trabajo.imgs[index];
-                          return Padding(
-                            padding: const EdgeInsets.only(bottom: 40),
-                            child: Image(
-                              image: NetworkImage(img),
-                              fit: BoxFit.cover,
-                            ),
-                          );
-                        },
-                      ),
-                    )),
+              if (widget.trabajo.imgs.isNotEmpty) _JobSwiper(widget: widget),
               if (widget.trabajo.video != '') ...[
                 const SizedBox(height: 20),
-                AspectRatio(
-                  aspectRatio: 16 / 9,
-                  child: ScreenPlayer(
-                    videoUrl: widget.trabajo.video!,
-                  ),
-                ),
+                _JobScreenPlayer(widget: widget),
               ],
               const SizedBox(height: 20),
               const Wrap(
                 children: [Tag(filterName: JobFilters.branding)],
               ),
               const SizedBox(height: 20),
-              Text(
-                widget.trabajo.title,
-                style: text22Title(context),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                widget.trabajo.shortDescription,
-                style: text14BodyM(context),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                widget.trabajo.description,
-                style: text14BodyM(context),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                'Fecha: 22-05-2021',
-                style: text14BodyM(context),
-              ),
+              _JobDialogInformation(trabajo: widget.trabajo),
               const SizedBox(height: 40),
               Container(
                 alignment: Alignment.center,
                 width: double.infinity,
                 child: const ActionButton(
                     text: 'Quiero algo así', tipo: AllServices.devWeb),
-              )
+              ),
+              const SizedBox(height: 20),
+              Row(children: [
+                const Spacer(),
+                IconButton(
+                  style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(bgColor)),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.close),
+                  color: Colors.red,
+                ),
+              ]),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class _JobDialogInformation extends StatelessWidget {
+  final Trabajo trabajo;
+  const _JobDialogInformation({required this.trabajo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          trabajo.title,
+          style: text22Title(context),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          trabajo.shortDescription,
+          style: text14BodyM(context),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          trabajo.description,
+          style: text14BodyM(context),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          'Fecha: 22-05-2021',
+          style: text14BodyM(context),
+        ),
+      ],
+    );
+  }
+}
+
+class _JobScreenPlayer extends StatelessWidget {
+  const _JobScreenPlayer({
+    super.key,
+    required this.widget,
+  });
+
+  final JobDialog widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: ScreenPlayer(
+        videoUrl: widget.trabajo.video!,
+      ),
+    );
+  }
+}
+
+class _JobSwiper extends StatelessWidget {
+  const _JobSwiper({
+    super.key,
+    required this.widget,
+  });
+
+  final JobDialog widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+        aspectRatio: 16 / 9,
+        child: SizedBox(
+          width: double.infinity,
+          height: 450,
+          child: Swiper(
+            autoplay: true,
+            autoplayDelay: 5000,
+            itemWidth: 400,
+            scrollDirection: Axis.horizontal,
+            itemCount: widget.trabajo.imgs.length,
+            pagination: const SwiperPagination(
+                builder: DotSwiperPaginationBuilder(
+                    activeColor: primaryColor, color: secondaryColor)),
+            itemBuilder: (context, index) {
+              final img = widget.trabajo.imgs[index];
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 40),
+                child: Image(
+                  image: NetworkImage(img),
+                  fit: BoxFit.cover,
+                ),
+              );
+            },
+          ),
+        ));
   }
 }
